ab0de73ae8420ab43178f2e9a6debab474d61096
Nghia <chrisnovlend@gmail.com>
2021-05-12 23:43:22 +0700

    del

---
+++ src/Components/BT3Content.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import BT3Carousel from './BT3Carousel'
+
+
+export default class BT3Content extends Component {
+    render() {
+        return (
+            <div>
+                <BT3Carousel/>
+            </div>
+        )
+    }
+}
---
+++ src/Components/BT3ProductItem.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export default class BT3ProductItem extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row text-center">
+                    <div className="col-lg-3 col-md-6 mb-4">
+                        <div className="card h-100">
+                            <img className="card-img-top" src="https://via.placeholder.com/500x325" alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                            </div>
+                            <div className="card-footer"><a className="btn btn-primary" href="#!">Find Out More!</a></div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 mb-4">
+                        <div className="card h-100">
+                            <img className="card-img-top" src="https://via.placeholder.com/500x325" alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+                            </div>
+                            <div className="card-footer"><a className="btn btn-primary" href="#!">Find Out More!</a></div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 mb-4">
+                        <div className="card h-100">
+                            <img className="card-img-top" src="https://via.placeholder.com/500x325" alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                            </div>
+                            <div className="card-footer"><a className="btn btn-primary" href="#!">Find Out More!</a></div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 mb-4">
+                        <div className="card h-100">
+                            <img className="card-img-top" src="https://via.placeholder.com/500x325" alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+                            </div>
+                            <div className="card-footer"><a className="btn btn-primary" href="#!">Find Out More!</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
